df924601f5b4e52d81d0d40273341ec4a5e04174
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-03-21 19:06:58 -0300

    Versionamento

--- Program.cs
+++ Program.cs
@@ -19,13 +19,26 @@ static void Menu()
         case 0 :System.Environment.Exit(0); break;
         case 1: Abrir(); break;
         case 2: Editar(); break;
+        //case 3: Salvar(); break;
         default: Menu(); break;
     }
 
 }
 static void Abrir()
 {
+    Console.Clear();
+    Console.WriteLine("Qual caminho do arquivo?");
+    string caminho = Console.ReadLine();
+
+    using(var arquivo = new StreamReader(caminho))
+    {
+        string texto = arquivo.ReadToEnd();
+        Console.WriteLine(texto);
+    }
+
     Console.WriteLine("");
+    Console.ReadLine();
+    Menu();
 }
 
 static void Editar()
@@ -36,10 +49,32 @@ static void Editar()
 
     string texto = "";
 
-    while (Console.ReadKey().Key != ConsoleKey.Escape)
+    do
     {
         texto += Console.ReadLine();
+        texto += Environment.NewLine;
+    }
+    while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+
+    Salvar(texto);
+}
+
+static void Salvar(string texto)
+{
+    Console.Clear();
+    Console.WriteLine("Qual local deseja salvar o arquivo?");
+
+    var caminho = Console.ReadLine(); //C:\Users\vinic\Desktop
+
+    using (var arquivo = new StreamWriter(caminho))
+    {
+        arquivo.Write(texto);
     }
 
-    Console.WriteLine(texto);
+    Console.WriteLine($"Arquivo{caminho} salvo com sucesso!");
+    Console.ReadLine();
+
+    Menu();
+
 }
