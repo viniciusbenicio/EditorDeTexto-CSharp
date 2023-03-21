4d845173ef16fe6df8bfd0b70f2e7fbeb3fadc32
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-03-21 18:46:18 -0300

    Versionamento

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+Menu();
+
+
+static void Menu()
+{
+
+    Console.Clear();
+    Console.WriteLine("O que você deseja fazer?");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Criar novo arquivo");
+    Console.WriteLine("0 - Sair");
+
+    short opcao = short.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 0 :System.Environment.Exit(0); break;
+        case 1: Abrir(); break;
+        case 2: Editar(); break;
+        default: Menu(); break;
+    }
+
+}
+    static void Abrir()
+    {
+
+    }
+
+    static void Editar()
+    {
+
+    }
